47e216173a92753acdc313b91aba2112d57f08fe
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-08-26 15:48:22 +0200

    first commit

---
+++ src/test/kotlin/com/codingdojo/kotlin/DemoTest.kt
@@ -0,0 +1,14 @@
+package com.codingdojo.kotlin
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class DemoTest {
+
+@Test
+fun `demo test`(){
+    val result = true
+    Assertions.assertThat(result).isTrue()
+ }
+    
+}
