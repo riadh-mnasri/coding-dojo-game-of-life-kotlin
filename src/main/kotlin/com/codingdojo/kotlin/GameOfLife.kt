3f53cca99a965238a90b50cb98e96936c511f857
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-08-26 17:17:22 +0200

    Add First Test

---
+++ src/main/kotlin/com/codingdojo/kotlin/GameOfLife.kt
@@ -0,0 +1,8 @@
+package com.codingdojo.kotlin
+
+class GameOfLife {
+    fun nexGeneration(arrayOf: Array<String>): Array<String> {
+       return arrayOf(".", ".", ".")
+    }
+
+}
---
+++ src/test/kotlin/com/codingdojo/kotlin/GameOfLifeTest.kt
@@ -0,0 +1,13 @@
+package com.codingdojo.kotlin
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class GameOfLifeTest {
+
+    @Test
+    fun `should cell with fewer than 2 neighbour die`(){
+        val gameOfLife = GameOfLife()
+        Assertions.assertThat(gameOfLife.nexGeneration(arrayOf("*", ".", "."))).isEqualTo(arrayOf(".", ".", "."))
+    }
+}
